30d5e453b1a679431888e1fa7d6520e19308f961
Emir Demirli <commandiron@gmail.com>
2022-09-25 20:27:36 +0300

    spacing added.

---
+++ app/src/main/java/com/commandiron/toprated10films/ui/theme/Spacing.kt
@@ -0,0 +1,28 @@
+package com.commandiron.toprated10films.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Spacing(
+    val default: Dp = 0.dp,
+    val spaceXXSmall: Dp = 2.dp,
+    val spaceExtraSmall: Dp = 4.dp,
+    val spaceSmall: Dp = 8.dp,
+    val spaceMedium: Dp = 16.dp,
+    val spaceLarge: Dp = 32.dp,
+    val spaceExtraLarge: Dp = 64.dp,
+    val spaceXXLarge: Dp = 128.dp,
+    val spaceXXXLarge: Dp = 256.dp,
+
+    val bottomNavHeight: Dp = 80.dp
+)
+
+val LocalSpacing = compositionLocalOf { Spacing() }
+
+val spacing: Spacing
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalSpacing.current
--- app/src/main/java/com/commandiron/toprated10films/ui/theme/Theme.kt
+++ app/src/main/java/com/commandiron/toprated10films/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
@@ -29,11 +30,13 @@ fun TopRated10FilmsTheme(
     } else {
         LightColorScheme
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
