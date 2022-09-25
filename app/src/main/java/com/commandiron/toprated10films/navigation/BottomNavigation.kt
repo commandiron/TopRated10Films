834e3d902e853bea560be0f2daf64a4175b41288
Emir Demirli <commandiron@gmail.com>
2022-09-26 02:12:38 +0300

    bottom navigation code opt.

--- app/src/main/java/com/commandiron/toprated10films/navigation/BottomNavigation.kt
+++ app/src/main/java/com/commandiron/toprated10films/navigation/BottomNavigation.kt
@@ -6,26 +6,26 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Tv
+import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.commandiron.toprated10films.R
 import com.commandiron.toprated10films.navigation.nav_graph.GraphItem
+import com.commandiron.toprated10films.ui.theme.NoRippleTheme
 import com.commandiron.toprated10films.ui.theme.spacing
 
 @Composable
@@ -56,34 +56,54 @@ fun BottomNavigation(
                 .background(Color.LightGray.copy(0.1f)),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            NavigationItem(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null
-                    ) { onNavItemClick(GraphItem.TopTenGraph.route) },
-                enabled = currentRoute != NavigationItem.WatchListScreen.route,
-                resourceId = if(currentRoute != NavigationItem.WatchListScreen.route) {
-                    R.drawable.app_logo_film
-                }else R.drawable.app_logo_film,
-                iconTint = Color.LightGray,
-                title = "Topten"
-            )
-            NavigationItem(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null
-                    ) { onNavItemClick(NavigationItem.WatchListScreen.route) },
-                enabled = currentRoute == NavigationItem.WatchListScreen.route,
-                iconVector = Icons.Default.Tv,
-                iconTint = Color.White,
-                title = "Watchlist"
-            )
+            CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
+                NavigationItem(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .clickable { onNavItemClick(GraphItem.TopTenGraph.route) },
+                    enabled = currentRoute != NavigationItem.WatchListScreen.route
+                ){
+                    Icon(
+                        modifier = Modifier.fillMaxHeight(0.45f),
+                        painter = painterResource(
+                            id = if(currentRoute != NavigationItem.WatchListScreen.route) {
+                                R.drawable.app_logo_film
+                            }else R.drawable.app_logo_film,
+                        ),
+                        contentDescription = null,
+                        tint = Color.Unspecified
+                    )
+                    Spacer(modifier = Modifier.width(MaterialTheme.spacing.spaceSmall))
+                    Text(
+                        text = "Topten",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+                NavigationItem(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .clickable { onNavItemClick(NavigationItem.WatchListScreen.route) },
+                    enabled = currentRoute == NavigationItem.WatchListScreen.route
+                ){
+                    Icon(
+                        modifier = Modifier.fillMaxHeight(0.45f),
+                        imageVector = Icons.Default.Tv,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(MaterialTheme.spacing.spaceSmall))
+                    Text(
+                        text = "Watchlist",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+            }
         }
     }
 }
@@ -92,15 +112,11 @@ fun BottomNavigation(
 fun NavigationItem(
     modifier: Modifier = Modifier,
     enabled: Boolean,
-    resourceId: Int? = null,
-    iconVector: ImageVector? = null,
-    iconTint: Color,
-    title: String,
+    content: @Composable RowScope.() -> Unit
 ) {
-    Column(
+    Box(
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        contentAlignment = Alignment.Center
     ) {
         Surface(
             modifier = Modifier
@@ -121,30 +137,7 @@ fun NavigationItem(
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                resourceId?.let {
-                    Icon(
-                        modifier = Modifier.fillMaxHeight(0.45f),
-                        painter = painterResource(id = it),
-                        contentDescription = null,
-                        tint = Color.Unspecified
-                    )
-
-                }
-                iconVector?.let {
-                    Icon(
-                        modifier = Modifier.fillMaxHeight(0.45f),
-                        imageVector = it,
-                        contentDescription = null,
-                        tint = iconTint
-                    )
-                }
-                Spacer(modifier = Modifier.width(MaterialTheme.spacing.spaceSmall))
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
+                content()
             }
         }
     }
